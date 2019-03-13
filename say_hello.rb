d01f3c19ff4bda23180104234dbbbec9f116864d
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-13 11:09:50 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
 
-def say_hello(name = "Ruby Programmer!")
-  puts "Hello #{name}"
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
   end
 
-say_hello("John!")
+say_hello("Kent Beck")
 
 say_hello
